3d5b861ac32f76593710f3db06f3eac72085f1de
TJ Vanderpoel <TJ Vanderpoel tj@rubyists.com>
2010-07-15 13:02:51 -0500

    made calls more flexible to handle freeswitch changing field names

--- lib/fsr/model/call.rb
+++ lib/fsr/model/call.rb
@@ -1,12 +1,11 @@
 module FSR
   module Model
     class Call
-      attr_reader :created, :created_epoch, :function, :caller_id_name, :caller_id_number,
-                  :caller_destination, :caller_channel_name, :caller_uuid, :callee_id_name,
-                  :callee_id_number, :callee_destination, :callee_channel_name, :callee_uuid, :hostname
+      attr_reader :fields
       def initialize(headers, *data)
-        headers.each_with_index do |h,i| 
-          instance_variable_set "@#{h}", data[i] 
+        @fields = headers
+        @fields.each_with_index do |h,i| 
+          (class << self; self; end).send(:define_method,h.to_sym) { data[i] }
         end
       end
     end
