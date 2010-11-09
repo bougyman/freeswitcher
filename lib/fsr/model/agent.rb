ba0cba719204d536a6e64b63d04bede559d16b15
TJ Vanderpoel <tj@rubyists.com>
2010-11-09 08:20:40 -0600

    added extension and full_name to Agent model

--- lib/fsr/model/agent.rb
+++ lib/fsr/model/agent.rb
@@ -1,12 +1,15 @@
 module FSR
   module Model
     class Agent
-      attr_reader :fields
+      attr_reader :fields, :extension, :full_name
+
       def initialize(headers, *data)
         @fields = headers
-        @fields.each_with_index do |h,i| 
+        @fields.each_with_index do |h,i|
           (class << self; self; end).send(:define_method,h.to_sym) { data[i] }
         end
+
+        @extension, @full_name = name.split("_", 2)
       end
     end
   end
