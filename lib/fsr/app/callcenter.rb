1d02afcc9f8050ce4b52f526b721ce828d257d1f
TJ Vanderpoel <tj@rubyists.com>
2011-02-16 16:36:23 -0600

    added callcenter app

---
+++ lib/fsr/app/callcenter.rb
@@ -0,0 +1,21 @@
+require "fsr/app"
+module FSR
+  module App
+    class Callcenter < Application
+      attr_reader :queue
+      def initialize(queue)
+        @queue = queue
+      end
+
+      def arguments
+        [queue]
+      end
+
+      def sendmsg
+        "call-command: execute\nexecute-app-name: %s\nexecute-app-arg: %s\nevent-lock:true\n\n" % [app_name, arguments.join(" ")]
+      end
+    end
+
+    register(:callcenter, CallCenter)
+  end
+end
