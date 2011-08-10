329da93b29a8b7f7e40f35ee3968d5684e319916
Michael Fellinger <m.fellinger@gmail.com>
2011-08-10 06:49:46 -0500

    Prettify play_and_get_digits example

--- examples/play_and_get_digits.rb
+++ examples/play_and_get_digits.rb
@@ -1,34 +1,26 @@
 #!/usr/bin/env ruby
 
-require File.join(File.dirname(__FILE__), "..", 'lib', 'fsr')
+require 'fsr'
 require "fsr/listener/outbound"
-$stdout.flush
+require 'securerandom'
 
 class PlayAndGetDigitsDemo < FSR::Listener::Outbound
+  SWEET_WAV = "/usr/local/freeswitch/sounds/music/8000/sweet.wav"
+  NOT_WAV = "/usr/local/freeswitch/sounds/music/8000/not.wav"
 
   def session_initiated
     exten = @session.headers[:caller_caller_id_number]
     FSR::Log.info "*** Answering incoming call from #{exten}"
 
     answer do
-      FSR::Log.info "***Reading DTMF from #{exten}"
-      #######################################################
-      ## NOTE YOU MUST MAKE SURE YOU PASS A VALID WAV FILE ##
-      #######################################################
-      play_and_get_digits("/usr/local/freeswitch/sounds/music/8000/sweet.wav","/usr/local/freeswitch/sounds/music/8000/not.wav", :chan_var => rand(10000).to_s) do |read_var|
-          FSR::Log.info "***Success, grabbed #{read_var.to_s.strip} from #{exten}"
-          # Tell the caller what they entered
-          # If you have mod_flite installed you should hear speech
-          speak("Got the DTMF of: #{read_var.to_s.strip}") { hangup }
+      FSR::Log.info "*** Reading DTMF from #{exten}"
+
+      play_and_get_digits(SWEET_WAV, NOT_WAV, chan_var: SecureRandom.uuid) do |digits|
+        FSR::Log.info "*** Success, grabbed #{digits.to_s.strip} from #{exten}"
+        speak("Got the DTMF of: #{digits.to_s.strip}") { hangup }
       end
     end
-
-    def receive_reply(reply)
-      FSR::Log.info "Received #{reply.inspect}"
-    end
-
   end
-
 end
 
-FSR.start_oes! PlayAndGetDigitsDemo, :port => 8084, :host => "0.0.0.0"
+FSR.start_oes! PlayAndGetDigitsDemo, port: 8084, host: "0.0.0.0"
